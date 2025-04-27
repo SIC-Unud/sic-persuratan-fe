4fe3ebdc76f2d3ba97ba2342309703cab06e9ebf
Surya Tanajaya <suryatanajaya006@gmail.com>
2025-04-27 16:10:03 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import AboutView from '@/views/AboutView.vue'
 import ManajemenSuratView from '@/views/ManajemenSuratView.vue'
+import PopUpHapus from '@/views/PopUpHapus.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -16,6 +17,11 @@ const router = createRouter({
       name: 'about',
       component: AboutView
     },
+    {
+      path: '/popup-hapus',
+      name: 'popup-hapus',
+      component: PopUpHapus
+    }
   ]
 })
 
