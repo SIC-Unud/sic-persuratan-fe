8ae75ee1196a888432689e4dbae39242d1c62d7d
aurell-ev <tinklerel@gmail.com>
2025-05-22 05:42:07 +0800

    style: responsive mobile ui

--- src/views/DetailSurat.vue
+++ src/views/DetailSurat.vue
@@ -1,36 +1,40 @@
 <template>
   <DashboardLayout>
     <div>
-      <div class="flex justify-between">
+      <div class="flex justify-between items-center">
         <div>
-          <h2 class="font-inter-sans text-xl font-bold text-content text-[28px]">
+          <h2 class="text-secondary font-inter-sans text-xl font-bold text-content text-[28px]">
           Detail Surat
         </h2>
         </div>
 
         <div class="flex justify-between">
-          <div class="flex gap-2">
-            <RouterLink to="#" class="px-4 py-2 bg-white text-primary text-xl font-jakarta-sans font-bold rounded-xl border border-solid border-primary border-1px hover:shadow-xl transition">Kembali</RouterLink>
-            <RouterLink to="#" class="px-4 py-2 bg-primary text-white text-xl font-jakarta-sans font-bold rounded-xl hover:shadow-xl transition">Update</RouterLink>
+          <div class="hidden md:flex justify-end gap-2 bg-white">
+            <RouterLink to="#" class="px-4 py-2 bg-white text-primary text-xl font-bold rounded-xl border border-primary hover:shadow-xl transition">
+              Kembali
+            </RouterLink>
+            <RouterLink to="#" class="px-6 py-2 bg-primary text-white text-xl font-bold rounded-xl hover:shadow-xl transition">
+              Update
+            </RouterLink>
           </div>
         </div>
       </div>
 
-      <form class="grid grid-cols-1 md:grid-cols-2 gap-6">
-        <div>
+      <div class="grid grid-cols-1 md:grid-cols-2 gap-6 p-3">
+        <div class="w-full">
           <label class="block text-base font-normal text-content">Nama Pengaju</label>
           <div class="p-3 bg-white rounded-md gap-2">
             <p class="font-medium text-black">Deyas</p>
           </div>
         </div>
-        <div>
+        <div class="w-full">
           <label class="block text-base font-normal text-content">Tema Kegiatan</label>
           <div class="p-3 bg-white rounded-md gap-2">
             <p class="font-medium text-black">Lomba</p>
           </div>
         </div>
 
-        <div>
+        <div class="w-full">
           <label class="block text-base font-normal text-content">Sumber Surat</label>
           <div class="p-3 bg-white rounded-md gap-2">
             <p class="font-medium text-black">Surat Panitia Pelaksana</p>
@@ -51,61 +55,66 @@
           </div>
         </div>
 
-        <div>
+        <div class="w-full">
           <label class="block text-base font-normal text-content">Jenis Surat</label>
           <div class="p-3 bg-white rounded-md gap-2">
             <p class="font-medium text-black">Surat Permohonan Izin Kegiatan</p>
           </div>
         </div>
-        <div>
+        <div class="w-full">
           <label class="block text-base font-normal text-content">Tempat Kegiatan</label>
           <div class="p-3 bg-white rounded-md gap-2">
             <p class="font-medium text-black">Gedung BG 1.1</p>
           </div>
         </div>
 
-        <div>
+        <div class="w-full">
           <label class="block text-base font-normal text-content">Nomor Surat</label>
           <div class="p-3 bg-white rounded-md gap-2">
             <p class="font-medium text-gray-200">Nomor surat belum ditentukan</p>
           </div>
         </div>
-        <div>
+        <div class="w-full">
           <label class="block text-base font-normal text-content">Status Surat</label>
           <div class="p-3 bg-white rounded-md gap-2">
             <p class="font-medium text-black">Menunggu Diajukan</p>
           </div>
         </div>
 
-        <div>
+        <div class="w-full">
           <label class="block text-base font-normal text-content">Tujuan Surat</label>
           <div class="p-3 bg-white rounded-md gap-2">
             <p class="font-medium text-black">Permohonan izin</p>
           </div>
         </div>
-        <div>
+        <div class="w-full">
           <label class="block text-base font-normal text-content">Keterangan</label>
           <div class="p-3 bg-white rounded-md gap-2">
             <p class="font-medium text-gray-200">Tidak ada keterangan</p>
           </div>
         </div>
 
-        <div>
+        <div class="w-full">
           <label class="block text-base font-normal text-content">Nama Kegiatan</label>
           <div class="p-3 bg-white rounded-md gap-2">
             <p class="font-medium text-black">SICKHATON</p>
           </div>
         </div>
-        <div>
+        <div class="w-full">
           <label class="block text-base font-normal text-content">Link pendukung</label>
           <div class="p-3 bg-white rounded-md gap-2">
             <p class="font-medium text-gray-200">Tidak ada link pendukung</p>
           </div>
-          
         </div>
-      </form>
-
-      
+      </div> 
+      <div class="flex md:hidden justify-end gap-3 bg-white py-6">
+        <RouterLink to="#" class="text-center px-4 py-2 bg-white text-primary text-xl font-bold rounded-xl border border-primary hover:shadow-xl transition">
+          Kembali
+        </RouterLink>
+        <RouterLink to="#" class="text-center px-4 py-2 bg-primary text-white text-xl font-bold rounded-xl hover:shadow-xl transition">
+          Update
+        </RouterLink>
+      </div>     
     </div>
   </DashboardLayout>
 
--- src/views/UpdateSurat.vue
+++ src/views/UpdateSurat.vue
@@ -1,106 +1,118 @@
 <template>
-    <DashboardLayout>
-      <div>
-        <div class="flex justify-between">
-          <div>
-            <h2 class="font-inter-sans text-xl font-bold text-content text-[28px]">
-            Detail Surat
-          </h2>
-          </div>
-  
-          <div class="flex justify-between">
-            <div class="flex gap-2">
-              <RouterLink to="#" class="px-4 py-2 bg-white text-primary text-xl font-jakarta-sans font-bold rounded-xl border border-solid border-primary border-1px hover:shadow-xl transition">Kembali</RouterLink>
-              <RouterLink to="#" class="px-4 py-2 bg-primary text-white text-xl font-jakarta-sans font-bold rounded-xl hover:shadow-xl transition">Simpan</RouterLink>
-            </div>
-          </div>
+  <DashboardLayout>
+    <div>
+      <div class="flex justify-between items-center">
+        <div>
+          <h2 class="font-inter-sans text-xl font-bold text-content text-[28px]">
+          Detail Surat
+        </h2>
         </div>
-  
-        <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
-          <div>
-            <label class="block text-base font-normal text-content">Nama Pengaju</label>
-            <input class="w-full p-2 border border-solid border-gray-300 border-1px rounded-lg gap-2"
-            placeholder="Nama pengaju">
-          </div>
-          <div>
-            <label class="block text-base font-normal text-content">Tema Kegiatan</label>
-            <input class="w-full p-2 border border-solid border-gray-300 border-1px rounded-lg gap-2"
-            placeholder="Tema kegiatan">
-          </div>
-  
-          <div>
-            <label class="block text-base font-normal text-content">Sumber Surat</label>
-            <input class="w-full p-2 border border-solid border-gray-300 border-1px rounded-lg gap-2"
-            placeholder="Sumber surat">
-          </div>
-          <div class="flex gap-6">
-            <div class="flex flex-col w-full">
-              <label class="text-base font-normal text-content mb-1">Tanggal</label>
-              <input class="w-full p-2 border border-solid border-gray-300 border-1px rounded-lg gap-2"
-            placeholder="Tanggal">
-            </div>
-            <div class="flex flex-col w-full">
-              <label class="text-base font-normal text-content mb-1">Pukul</label>
-              <input class="w-full p-2 border border-solid border-gray-300 border-1px rounded-lg gap-2"
-            placeholder="Pukul">
-            </div>
-          </div>
-  
-          <div>
-            <label class="block text-base font-normal text-content">Jenis Surat</label>
-            <input class="w-full p-2 border border-solid border-gray-300 border-1px rounded-lg gap-2"
-            placeholder="Jenis surat">
-          </div>
-          <div>
-            <label class="block text-base font-normal text-content">Tempat Kegiatan</label>
-            <input class="w-full p-2 border border-solid border-gray-300 border-1px rounded-lg gap-2"
-            placeholder="Nama gedung, ruangan">
-          </div>
-  
-          <div>
-            <label class="block text-base font-normal text-content">Nomor Surat</label>
-            <input class="w-full p-2 border border-solid border-gray-300 border-1px rounded-lg gap-2"
-            placeholder="Nomor surat">
-          </div>
-          <div>
-            <label class="block text-base font-normal text-content">Status Surat</label>
-            <select class="w-full p-2 border border-solid border-gray-300 border-1px rounded-lg gap-2">
-                <option value="Menunggu Diajukan">Menunggu Diajukan</option>
-                <option value="Sedang Diajukan">Sedang Diajukan</option>
-                <option value="Berhasil Diajukan">Berhasil Diajukan</option>
-                <option value="Ditolak">Ditolak</option>
-                <option value="Dibatalkan">Dibatalkan</option>
-              </select>
-          </div>
-  
-          <div>
-            <label class="block text-base font-normal text-content">Tujuan Surat</label>
-            <input class="w-full p-2 border border-solid border-gray-300 border-1px rounded-lg gap-2"
-            placeholder="Tujuan">
-          </div>
-          <div>
-            <label class="block text-base font-normal text-content">Keterangan</label>
-            <input class="w-full p-2 border border-solid border-gray-300 border-1px rounded-lg gap-2"
-            placeholder="Keterangan">
-          </div>
-  
-          <div>
-            <label class="block text-base font-normal text-content">Nama Kegiatan</label>
-            <input class="w-full p-2 border border-solid border-gray-300 border-1px rounded-lg gap-2"
-            placeholder="Nama kegiatan">
-          </div>
-          <div>
-            <label class="block text-base font-normal text-content">Link pendukung</label>
-            <input class="w-full p-2 border border-solid border-gray-300 border-1px rounded-lg gap-2"
-            placeholder="Link">
+
+        <div class="flex justify-between">
+          <div class="hidden md:flex justify-end gap-2 bg-white">
+            <RouterLink to="#" class="px-4 py-2 bg-white text-primary text-xl font-bold rounded-xl border border-primary hover:shadow-xl transition">
+              Batal
+            </RouterLink>
+            <RouterLink to="#" class="px-6 py-2 bg-primary text-white text-xl font-bold rounded-xl hover:shadow-xl transition">
+              Simpan
+            </RouterLink>
           </div>
         </div>
       </div>
-    </DashboardLayout>
-  </template>
-  
-  <script setup>
-  import DashboardLayout from '@/layout/DashboardLayout.vue';
-  import NavItem from '@/components/NavItem.vue'
-  
-  </script>
+
+      <form class="grid grid-cols-1 md:grid-cols-2 gap-6 overflow-y-auto">
+        <div>
+          <label class="block text-base font-normal text-content">Nama Pengaju</label>
+          <input class="w-full p-2 border border-solid border-gray-300 border-1px rounded-lg gap-2"
+          placeholder="Nama pengaju">
+        </div>
+        <div>
+          <label class="block text-base font-normal text-content">Tema Kegiatan</label>
+          <input class="w-full p-2 border border-solid border-gray-300 border-1px rounded-lg gap-2"
+          placeholder="Tema kegiatan">
+        </div>
+
+        <div>
+          <label class="block text-base font-normal text-content">Sumber Surat</label>
+          <input class="w-full p-2 border border-solid border-gray-300 border-1px rounded-lg gap-2"
+          placeholder="Sumber surat">
+        </div>
+        <div class="flex gap-6">
+          <div class="flex flex-col w-full">
+            <label class="text-base font-normal text-content mb-1">Tanggal</label>
+            <input type="date" class="w-full p-2 border border-solid border-gray-300 border-1px rounded-lg gap-2"
+          placeholder="Tanggal">
+          </div>
+          <div class="flex flex-col w-full">
+            <label class="text-base font-normal text-content mb-1">Pukul</label>
+            <input class="w-full p-2 border border-solid border-gray-300 border-1px rounded-lg gap-2"
+          placeholder="Pukul">
+          </div>
+        </div>
+
+        <div>
+          <label class="block text-base font-normal text-content">Jenis Surat</label>
+          <input class="w-full p-2 border border-solid border-gray-300 border-1px rounded-lg gap-2"
+          placeholder="Jenis surat">
+        </div>
+        <div>
+          <label class="block text-base font-normal text-content">Tempat Kegiatan</label>
+          <input class="w-full p-2 border border-solid border-gray-300 border-1px rounded-lg gap-2"
+          placeholder="Nama gedung, ruangan">
+        </div>
+
+        <div>
+          <label class="block text-base font-normal text-content">Nomor Surat</label>
+          <input class="w-full p-2 border border-solid border-gray-300 border-1px rounded-lg gap-2"
+          placeholder="Nomor surat">
+        </div>
+        <div>
+          <label class="block text-base font-normal text-content">Status Surat</label>
+          <select class="w-full p-2 border border-solid border-gray-300 border-1px rounded-lg gap-2">
+              <option value="Menunggu Diajukan">Menunggu Diajukan</option>
+              <option value="Sedang Diajukan">Sedang Diajukan</option>
+              <option value="Berhasil Diajukan">Berhasil Diajukan</option>
+              <option value="Ditolak">Ditolak</option>
+              <option value="Dibatalkan">Dibatalkan</option>
+            </select>
+        </div>
+
+        <div>
+          <label class="block text-base font-normal text-content">Tujuan Surat</label>
+          <input class="w-full p-2 border border-solid border-gray-300 border-1px rounded-lg gap-2"
+          placeholder="Tujuan">
+        </div>
+        <div>
+          <label class="block text-base font-normal text-content">Keterangan</label>
+          <input class="w-full p-2 border border-solid border-gray-300 border-1px rounded-lg gap-2"
+          placeholder="Keterangan">
+        </div>
+
+        <div>
+          <label class="block text-base font-normal text-content">Nama Kegiatan</label>
+          <input class="w-full p-2 border border-solid border-gray-300 border-1px rounded-lg gap-2"
+          placeholder="Nama kegiatan">
+        </div>
+        <div>
+          <label class="block text-base font-normal text-content">Link pendukung</label>
+          <input class="w-full p-2 border border-solid border-gray-300 border-1px rounded-lg gap-2"
+          placeholder="Link">
+        </div>
+      </form>
+      <div class="flex md:hidden justify-end gap-3 bg-white py-6">
+        <RouterLink to="#" class="text-center px-4 py-2 bg-white text-primary text-xl font-bold rounded-xl border border-primary hover:shadow-xl transition">
+          Kembali
+        </RouterLink>
+        <RouterLink to="#" class="text-center px-4 py-2 bg-primary text-white text-xl font-bold rounded-xl hover:shadow-xl transition">
+          Simpan
+        </RouterLink>
+      </div>
+    </div>
+  </DashboardLayout>
+</template>
+
+<script setup>
+import DashboardLayout from '@/layout/DashboardLayout.vue';
+import NavItem from '@/components/NavItem.vue'
+
+</script>
