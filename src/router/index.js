cce684ddbc0780793cffc8aceefcfb030a4cd006
Surya Tanajaya <suryatanajaya006@gmail.com>
2025-07-18 00:42:25 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -8,9 +8,9 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
+      path: '/detail-surat',
       name: 'detail-surat',
-      component: DetailSurat
+      component: DetailSuratView
     },
     {
       path: '/about',
