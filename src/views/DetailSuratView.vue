46d191f8ad24fcbb07837bd31cd09a8df1ab4add
Surya Tanajaya <suryatanajaya006@gmail.com>
2025-07-18 00:43:12 +0800

    Update DetailSuratView.vue

---
+++ src/views/DetailSuratView.vue
@@ -0,0 +1,10 @@
+<template>
+    <DashboardLayout>
+        <DetailSurat></DetailSurat>
+    </DashboardLayout>
+</template>
+
+<script setup>
+import DashboardLayout from '@/layout/DashboardLayout.vue';
+import DetailSurat from '@/layout/DetailSurat.vue';
+</script>
